c2ab030102503f47c203e0b8784fe3ca0dbb4a27
Tahir Fazal <tahirawan4@gmail.com>
2018-07-01 15:23:15 +0500

    Fixing issues

--- age_estimation/forms.py
+++ age_estimation/forms.py
@@ -11,24 +11,25 @@ class UserSignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'password1', 'password2', 'gender', 'age', 'country',
-                  'frequency1', 'frequency2', 'average']
+        fields = ['username', 'first_name', 'last_name', 'password1', 'password2', 'gender', 'age', 'country']
 
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First name'}))
-    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'Last name'}))
-    password1 = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'Password1'}))
-    password2 = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'Password2'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last name'}))
+    password1 = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Password1'}))
+    password2 = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Password2'}))
 
     gender = forms.ChoiceField(choices=GENDER_CHOICES)
-    age = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'Age'}))
-    country = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'Country'}))
+    age = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Age'}))
+    country = forms.CharField(max_length=50,
+                              widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Country'}))
+
     # frequency1 = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     # frequency2 = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     # average = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    frequency1 = forms.CharField(widget=forms.HiddenInput())
-    frequency2 = forms.CharField(widget=forms.HiddenInput())
-    average = forms.CharField(widget=forms.HiddenInput())
+    # frequency1 = forms.CharField(widget=forms.HiddenInput())
+    # frequency2 = forms.CharField(widget=forms.HiddenInput())
+    # average = forms.CharField(widget=forms.HiddenInput())
 
     def __init__(self, *args, **kwargs):
         super(UserSignUpForm, self).__init__(*args, **kwargs)
@@ -40,6 +41,12 @@ class UserSignUpForm(UserCreationForm):
         if User.objects.filter(username__iexact=username).exists():
             raise forms.ValidationError("Username already exist")
         return username
+
+    def clean_age(self):
+        age = self.cleaned_data['age']
+        if age > 18:
+            raise forms.ValidationError("Age can't be more than 18.")
+        return age
 
 
 class UserLoginForm(AuthenticationForm):
